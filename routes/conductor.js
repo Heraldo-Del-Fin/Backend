ea3ed841290c834808f92ef7931bd7f6c24a1435
Heraldo Del Fin <lserratoprincipe@correo.unicordoba.edu.co>
2024-11-30 18:13:22 -0500

    otra actualizacion

--- routes/conductor.js
+++ routes/conductor.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const { getConductores, createConductor } = require('../controllers/conductorController');
-const { body } = require('express-validator');
+const express = require("express");
+const {
+  getConductores,
+  createConductor,
+} = require("../controllers/conductorController");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
 // Ruta para obtener todos los conductores
-router.get('/', getConductores);
+router.get("/", getConductores);
 
 // Ruta para crear un nuevo conductor
-router.post('/', [
-    body('ID_conductor').isInt().withMessage('El ID debe ser un número entero'),
-    body('nombre_apellido').isString().notEmpty().withMessage('El nombre es obligatorio'),
-    body('licencia').isString().notEmpty().withMessage('La licencia es obligatoria'),
+router.post(
+  "/",
+  [
+    body("ID_conductor").isInt().withMessage("El ID debe ser un número entero"),
+    body("nombre_apellido")
+      .isString()
+      .notEmpty()
+      .withMessage("El nombre es obligatorio"),
+    body("licencia")
+      .isString()
+      .notEmpty()
+      .withMessage("La licencia es obligatoria"),
   ],
-  createConductor);
+  createConductor
+);
 
 module.exports = router;
